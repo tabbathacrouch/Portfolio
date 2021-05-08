2cb6d89468ade81db815e18194254aa480c31464
Tabbatha Crouch <tabbathacrouch@gmail.com>
2021-05-07 19:50:21 -0500

    added project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,24 +1,26 @@
 import React from "react";
 import CardTilt from "./CardTilt.js";
 import "./Projects.css";
-import onlineTest from './assets/online-test.png';
+import onlineTest from "./assets/online-test.png";
 import faceDetection from "./assets/faceDetection.png";
 import birthdayCard from "./assets/birthday card.png";
-import robofriend from "./assets/robofriend.png";
-
+import mathOperations from "./assets/keys.jpg";
 
 function Projects() {
   return (
     <div id="projects">
       <h2>Projects</h2>
       <div className="projects-container">
-      <div className="row">
+        <div className="row">
           <div className="project-text">
             <p className="project-title">Math Test Generator</p>
             <div>
               <p className="project-info">
-                Create a test with multiple choice, short answer, or check box style questions.<br/>Once a test is created, share the link and test code.
-                &nbsp;&nbsp;View reponses and metrics from each test you create in your personalized dashboard.&nbsp;&nbsp;Register as a user today! 
+                Create a test with multiple choice, short answer, or check box
+                style questions.&nbsp;&nbsp;Once a test is created, share the
+                link and test code.&nbsp;&nbsp;View reponses and metrics from
+                each test you create in your personalized
+                dashboard.&nbsp;&nbsp;Register as a user today!
               </p>
             </div>
             <a
@@ -45,22 +47,29 @@ function Projects() {
               target="_blank"
               rel="noreferrer"
             >
-              <img
-                className="project-image"
-                src={onlineTest}
-                alt="project 1"
-              />
+              <img className="project-image" src={onlineTest} alt="project 1" />
             </a>
           </div>
         </div>
+
         <div className="row">
           <div className="project-text">
-            <p className="project-title">Face Detection App</p>
+            <p className="project-title">Math is Beautiful</p>
             <div>
               <p className="project-info">
-                Sign in or register a user and detect faces using a Clarifai
-                API. Simply enter the url of an image and watch the API locate
-                the face and display a blue rectangle.
+                After spending more than 4 years creating mathematics content
+                for an ed-tech company, I decided to create a free math resource
+                similar to the{" "}
+                <a
+                  href="https://www.mathsisfun.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  mathisfun.com
+                </a>{" "}
+                website.&nbsp;&nbsp;This resource contains an interactive number
+                chart, embedded Phet&trade; simulations, and a searchable
+                glossary.
               </p>
             </div>
             <a
@@ -73,14 +82,13 @@ function Projects() {
             </a>
             <a
               className="source-code"
-              href="https://github.com/tabbathacrouch/facerecognition_frontend"
+              href="https://github.com/tabbathacrouch/Math-is-Beautiful"
               target="_blank"
               rel="noreferrer"
             >
               Source Code
             </a>
           </div>
-
           <div className="card" onMouseOver={CardTilt}>
             <a
               href="https://peaceful-plains-96573.herokuapp.com"
@@ -89,7 +97,7 @@ function Projects() {
             >
               <img
                 className="project-image"
-                src={faceDetection}
+                src={mathOperations}
                 alt="project 1"
               />
             </a>
@@ -101,10 +109,10 @@ function Projects() {
             <p className="project-title">Birthday Cards</p>
             <div>
               <p className="project-info">
-                I love sending birthday cards to friends and family members! I
-                created an app that helps me keep track of all their birthdays,
-                generates number facts using an API, and creates a birthday card
-                using an printable template.
+                I love sending birthday cards to friends and family
+                members!&nbsp;&nbsp;I created an app that helps me keep track of
+                all their birthdays, generates number facts using an API, and
+                creates a birthday card using an printable template.
               </p>
             </div>
             <a
@@ -141,10 +149,12 @@ function Projects() {
 
         <div className="row">
           <div className="project-text">
-            <p className="project-title">Robofriends</p>
+            <p className="project-title">Face Detection App</p>
             <div>
               <p className="project-info">
-                Search through a list of robofriends.
+                Sign in or register a user and detect faces using a Clarifai
+                API. Simply enter the url of an image and watch the API locate
+                the face and display a blue rectangle.
               </p>
             </div>
             <a
@@ -157,7 +167,7 @@ function Projects() {
             </a>
             <a
               className="source-code"
-              href="https://github.com/tabbathacrouch/Robofriends"
+              href="https://github.com/tabbathacrouch/facerecognition_frontend"
               target="_blank"
               rel="noreferrer"
             >
@@ -170,7 +180,11 @@ function Projects() {
               target="_blank"
               rel="noreferrer"
             >
-              <img className="project-image" src={robofriend} alt="project 1" />
+              <img
+                className="project-image"
+                src={faceDetection}
+                alt="project 1"
+              />
             </a>
           </div>
         </div>
