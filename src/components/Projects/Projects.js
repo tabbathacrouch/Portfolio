63a5e1e46c02aa24cb24aae929ef261bcdff008e
Tabbatha Crouch <tabbathacrouch@gmail.com>
2021-08-12 16:34:11 -0500

    update projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,9 +2,9 @@ import React from "react";
 import CardTilt from "./CardTilt.js";
 import "./Projects.css";
 import onlineTest from "./assets/online-test.png";
-import faceDetection from "./assets/faceDetection.png";
-import mathOperations from "./assets/keys.jpg";
 import garden from "./assets/garden.jpeg";
+import numberChart from "./assets/numberChart.jpeg";
+import trivia from "./assets/trivia.jpeg";
 
 function Projects() {
   return (
@@ -13,7 +13,9 @@ function Projects() {
       <div className="projects-container">
         <div className="row">
           <div className="project-text">
-            <div className="project-title">Math Test Generator</div>
+            <div className="project-title">
+              Math Test Generator (UNDER CONSTRUCTION)
+            </div>
             <div>
               <div className="project-info">
                 Create a test with multiple choice, short answer, or check box
@@ -61,7 +63,7 @@ function Projects() {
 
         <div className="row">
           <div className="project-text">
-            <div className="project-title">Gardening</div>
+            <div className="project-title">Gardening (UNDER CONSTRUCTION)</div>
             <div>
               <div className="project-info">
                 This React app utilizes Bootstrap (v5.0) and fetches information
@@ -108,26 +110,20 @@ function Projects() {
 
         <div className="row">
           <div className="project-text">
-            <div className="project-title">Math is Beautiful</div>
+            <div className="project-title">Trivia (UNDER CONSTRUCTION)</div>
             <div>
               <div className="project-info">
-                After spending more than 4 years creating mathematics content
-                for an ed-tech company, I decided to create a free math resource
-                similar to the{" "}
-                <a
-                  href="https://www.mathsisfun.com/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  mathisfun.com
-                </a>{" "}
-                website. This resource contains an interactive number chart,
-                embedded Phet&trade; simulations, and a searchable glossary.
+                Select a category, fetch trivia questions and answers from{" "}
+                <a href="https://opentdb.com/api_config.php">
+                  Open Trivia Database
+                </a>
+                , and see a live score. Grab your friends and play along
+                together!
               </div>
             </div>
             <a
               className="see-live"
-              href="https://ancient-garden-59060.herokuapp.com/"
+              href="https://trivia-67600.herokuapp.com/"
               target="_blank"
               rel="noreferrer"
             >
@@ -135,7 +131,7 @@ function Projects() {
             </a>
             <a
               className="source-code"
-              href="https://github.com/tabbathacrouch/Math-is-Beautiful"
+              href="https://github.com/tabbathacrouch/trivia"
               target="_blank"
               rel="noreferrer"
             >
@@ -144,16 +140,16 @@ function Projects() {
           </div>
           <div className="card" onMouseOver={CardTilt}>
             <a
-              href="https://ancient-garden-59060.herokuapp.com/"
+              href="https://trivia-67600.herokuapp.com/"
               target="_blank"
               rel="noreferrer"
             >
               <img
                 className="project-image"
-                src={mathOperations}
+                src={trivia}
                 alt="project 1"
                 width="192px"
-                height="192px"
+                height="153px"
               />
             </a>
           </div>
@@ -161,25 +157,19 @@ function Projects() {
 
         <div className="row">
           <div className="project-text">
-            <div className="project-title">Face Detection App</div>
+            <div className="project-title">Number Chart</div>
             <div>
               <div className="project-info">
-                Sign in or register a user and detect faces in an image using
-                the{" "}
-                <a
-                  href="https://www.clarifai.com/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Clarifai
-                </a>{" "}
-                API. Simply enter the url of an image and watch the API detect
-                the faces.
+                A single page react app that utilizes the useState hook to
+                manage the state of an interactive number chart. Select the size
+                (1-20, 1-50, or 1-100) and a color to illustrate patterns.
+                Additionally, use the 'multiples' buttons to reveal the
+                multiples of 2, 5, or 10.
               </div>
             </div>
             <a
               className="see-live"
-              href="https://peaceful-plains-96573.herokuapp.com"
+              href="https://number-chart-59060.herokuapp.com/"
               target="_blank"
               rel="noreferrer"
             >
@@ -187,7 +177,7 @@ function Projects() {
             </a>
             <a
               className="source-code"
-              href="https://github.com/tabbathacrouch/facerecognition_frontend"
+              href="https://github.com/tabbathacrouch/number-chart"
               target="_blank"
               rel="noreferrer"
             >
@@ -196,15 +186,15 @@ function Projects() {
           </div>
           <div className="card" onMouseOver={CardTilt}>
             <a
-              href="https://peaceful-plains-96573.herokuapp.com"
+              href="https://number-chart-59060.herokuapp.com/"
               target="_blank"
               rel="noreferrer"
             >
               <img
                 className="project-image"
-                src={faceDetection}
-                alt="project 1"
-                width="192px"
+                src={numberChart}
+                alt="number chart"
+                width="152px"
                 height="192px"
               />
             </a>
