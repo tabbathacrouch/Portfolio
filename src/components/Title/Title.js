a87fdee78722895e9eeb91e981ffd49fa73bb79e
Tabbatha Crouch <tabbathacrouch@gmail.com>
2022-06-13 16:59:02 -0500

    title section updates

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,28 +1,36 @@
 import React from "react";
 import { Link } from "react-scroll";
+import profile from "./profile.jpeg";
+import { MdOutlineEmail } from "react-icons/md";
+import { IoInformationCircleOutline } from "react-icons/io5";
 import "./Title.css";
 
 function Title() {
   return (
     <div id="title">
-      <div>
+      <div className="title-container">
+        <div className="title-profile-img">
+          <img src={profile} alt="profile - tabbatha crouch" />
+        </div>
         <div className="title-sectionTitle">
           Hey there, my name is Tabbatha Crouch.
           <br />
           I'm a Software Developer.
         </div>
-        <div className="buttons-title">
-          <Link to="about" activeClass="active" spy={true} smooth={true}>
-            <button type="button" className="title-btn">
-              Learn more
-            </button>
-          </Link>
-          <Link to="contact" activeClass="active" spy={true} smooth={true}>
-            <button type="button" className="title-btn">
-              Contact me
-            </button>
-          </Link>
-        </div>
+      </div>
+      <div className="buttons-title">
+        <Link to="about" activeClass="active" spy={true} smooth={true}>
+          <button type="button" className="title-btn">
+            <IoInformationCircleOutline style={{ paddingRight: "2vmin" }} />
+            Learn more
+          </button>
+        </Link>
+        <Link to="contact" activeClass="active" spy={true} smooth={true}>
+          <button type="button" className="title-btn">
+            <MdOutlineEmail style={{ paddingRight: "2vmin" }} />
+            Contact me
+          </button>
+        </Link>
       </div>
     </div>
   );
